f8ad52b6efdd2d57c2c5e8fcbd0431867045e880
Anastas Mironov <ast@l80.ru>
2024-07-05 02:01:54 +0900

    fix: laravel-data version ^4.5

--- src/UI/API/DTO/InfographicDTO.php
+++ src/UI/API/DTO/InfographicDTO.php
@@ -4,10 +4,13 @@ namespace AdminKit\Infographics\UI\API\DTO;
 
 use AdminKit\Infographics\Models\Infographic;
 use Illuminate\Support\Collection;
+use Spatie\LaravelData\Concerns\WithDeprecatedCollectionMethod;
 use Spatie\LaravelData\Data;
 
 class InfographicDTO extends Data
 {
+    use WithDeprecatedCollectionMethod;
+
     public function __construct(
         public string $background,
         public array $action,
